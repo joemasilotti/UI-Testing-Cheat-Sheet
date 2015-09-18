82cd0bbaa0eca600f5c2f4a88d4573289efd5b3b
Joe Masilotti <joseph.masilotti@gmail.com>
2015-09-18 16:35:09 -0400

    Update helper method to report better line numbers

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -134,9 +134,13 @@ class UITests: XCTestCase {
         XCTAssert(app.navigationBars["Volleyball?"].exists)
     }
 
-    private func waitForElementToAppear(element: XCUIElement) {
+    private func waitForElementToAppear(element: XCUIElement, file: String = __FILE__, line: UInt = __LINE__) {
         let existsPredicate = NSPredicate(format: "exists == true")
         expectationForPredicate(existsPredicate, evaluatedWithObject: element, handler: nil)
-        waitForExpectationsWithTimeout(5, handler: nil)
+
+        waitForExpectationsWithTimeout(5) { (error) -> Void in
+            let message = "Failed to find \(element) after 5 seconds."
+            self.recordFailureWithDescription(message, inFile: file, atLine: line, expected: true)
+        }
     }
 }
